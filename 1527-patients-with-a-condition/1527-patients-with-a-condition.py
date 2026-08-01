137c7102e8a998e1747bb687bbc755831396ec4e
KRISH SHARMA <cozykrish2916@gmail.com>
2026-08-01 23:31:54 +0530

    Time: 287 ms (31.19%), Space: 66.7 MB (37.1%) - LeetHub

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.py
@@ -0,0 +1,4 @@
+import pandas as pd
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    pattern = r'(^| )DIAB1\w*'
+    return patients[patients["conditions"].str.contains(pattern, regex=True, na=False)]
