adf318eedac1d5acc51c3bf1d7269d2ef398dd2e
KRISH SHARMA <cozykrish2916@gmail.com>
2026-09-21 18:39:20 +0530

    Time: 4 ms (95.37%), Space: 44.7 MB (86.27%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        String[] parts = path.split("/");
+        for (String part : parts) {
+            if (part.isEmpty() || part.equals(".")) {
+                continue;
+            }
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pollLast();
+                }
+            } else {
+                stack.offerLast(part);
+            }
+        }
+        if (stack.isEmpty()) {
+            return "/";
+       }
+
+        StringBuilder sb = new StringBuilder();
+        for (String dir : stack) {
+            sb.append("/").append(dir);
+        }
+        return sb.toString();
+    }
+}
